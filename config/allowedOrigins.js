ac1ca3fe0c6c53ce7177d358294062e3d032972e
sahilsapariya <sahilsapariya03@gmail.com>
2023-05-17 01:39:05 +0530

    created middleware logger and errorhandler

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.dandrepairshop.com',
+    'https://dandrepaishop.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const cookieParser = require('cookie-parser')
+const errorHandler = require('./middleware/errorHandler')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
+app.use(logger)
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(cors())
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static('public'))
 
 app.use('/', require('./routes/root'))
 
+
+// setting route for 404 Error
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
@@ -21,5 +35,6 @@ app.all('*', (req, res) => {
     }
 })
 
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
