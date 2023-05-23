e0cab79aaabeefa09a67e43d11d81fd9ec199015
sahilsapariya <sahilsapariya03@gmail.com>
2023-05-23 17:14:36 +0530

    initial commit

---
+++ Course 1/db.py
@@ -0,0 +1,30 @@
+stores = {
+    "1": {
+        "name": "JC Tyres",
+        "location": "Jamjodhpur",
+        "items": ["1", "2"]
+    },
+    "2": {
+        "name": "Poonam Pan",
+        "location" : "Rajkot",
+        "items" : ["3", "5"]
+    }
+}
+items = {
+    "1": {
+        "name": "SONALICA",
+        "price": 107.99
+    },    
+    "2": {
+        "name": "F21 2.75.00-18",
+        "price": 49.99
+    },    
+    "3": {
+        "name": "Masala Paan",
+        "price": 10.00
+    },    
+    "5": {
+        "name": "Cigarate",
+        "price": 9.49
+    },    
+}
