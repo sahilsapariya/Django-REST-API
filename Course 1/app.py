d41bb92c7fd33b4091790271a822111032f986d4
sahilsapariya <sahilsapariya03@gmail.com>
2023-05-23 23:34:43 +0530

    added flask-smorest for more efficient development

--- Course 1/app.py
+++ Course 1/app.py
@@ -1,111 +1,22 @@
-from flask import Flask, request
-from flask_cors import CORS
-from db import stores, items
-from flask_smorest import abort
-import uuid
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+
 
 app = Flask(__name__)
-CORS(app)
 
-@app.get("/stores")
-def get_stores():
-    return {"stores": list(stores.values())}
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URI"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
+api = Api(app)
 
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if (
-        "price" not in store_data
-    ):
-        abort(
-            400,
-            message="Bad Request. Ensure 'name' is included in JSON playload"
-        )
-    for store in stores.values():
-        if store["name"] == store['name']:
-            abort(404, message=f"{store['name']} already exists.")
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    return store, 201
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
 
-
-@app.post('/item')
-def create_item():
-    item_data = request.get_json()
-    """Here not only we need to validate data exits,
-    But also what types of data. Price should be a float,
-    for example."""
-    if (
-        "price" not in item_data
-        or "store_id" not in item_data
-        or "name" not in item_data
-    ):
-        abort(
-            400,
-            message="Bad Request. Ensure 'price', 'store_id', and 'name' are included in JSON playload"
-        )
-    for item in items.values():
-        if (
-            item_data["name"] == item['name']
-            and item_data["store_id"] == item["store_id"]
-        ):
-            abort(404, message=f"{item} already exists.")
-    if item_data['store_id'] not in stores:
-        abort(404, message="Store not found")
-
-
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id": item_id}
-    items[item_id] = item
-
-    return item, 201
-
-
-@app.get("/stores/<store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found")
-
-@app.get("/item/<item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Store not found")
-        
-@app.get("/items")
-def get_items():
-    try:
-        return items
-    
-    except KeyError:
-        abort(404, message="items not found")
-
-
-@app.delete("/item/<item_id>")
-def delete_item(item_id):
-    try:
-        del items[item_id]
-        return {"message": "Item deleted."}
-    except KeyError:
-        abort(404, message="Store not found")
-
-@app.put("/item/<item_id>")
-def update_item(item_id):
-    item_data = request.get_json()
-    if "price" not in item_data or "name" not in item_data:
-        abort(404, message="Bad request. Ensure 'price', and 'name' are included in JSON playload.")
-
-    try:
-        item = items[item_id]
-        item |= item_data
-
-        return item 
-    except KeyError:
-        abort(404, message="Item not found.")
-
-app.run(debug=True)
